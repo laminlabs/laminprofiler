bc99f379da2f4496cf7077ad8a3a1e6b17b78731
Alex Wolf <f.alexander.wolf@gmail.com>
2026-02-02 23:00:09 +0100

    🔖 Release 0.1.0

--- laminprofiler/__init__.py
+++ laminprofiler/__init__.py
@@ -1,3 +1,3 @@
 """Simple analytics for GitHub contributions across an organization."""
 
-__version__ = "0.0.2"  # denote a pre-release for 0.1.0 with 0.1rc1
+__version__ = "0.1.0"  # denote a pre-release for 0.1.0 with 0.1rc1
